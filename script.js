8e9fd80bbdad336e697bc7c64f4298670cbd1590
Jan Nosek <100805713+S1ngleWasTaken@users.noreply.github.com>
2023-09-27 07:38:04 +0200

    finished

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 const gameboard = () => {
     const gridSpots = document.querySelectorAll('.grid-spot')
     const restartBtn = document.getElementById('restart-btn')
+    const heading = document.getElementById('heading')
     let gridSpotValues = ['', '', '', '', '', '', '', '', '']
     let playerSwitcher = 1; // 1 == X | -1 == O
-    let playCounter = 0;
     let finished = false;
+    let playCounter = 0;
     const restartGame = () => {
         gridSpotValues = ['', '', '', '', '', '', '', '', '']
         playerSwitcher = 1;
@@ -15,6 +16,7 @@ const gameboard = () => {
             gridSpot.textContent = ''
             gridSpot.style.backgroundColor = 'rgb(77, 77, 77)'
         })
+        heading.textContent = '';
         console.log('Game was restarted');
     }
     restartBtn.addEventListener('click', restartGame)
@@ -22,7 +24,7 @@ const gameboard = () => {
         for (let i = 0; i < 9; i += 3) {
             if (gridSpotValues[i] != '' && gridSpotValues[i] == gridSpotValues[i + 1] && gridSpotValues[i + 1] == gridSpotValues[i + 2]) {
 
-
+                heading.textContent = `${gridSpotValues[i]} won!`
                 gridSpots[i].style.backgroundColor = 'rgb(102, 102, 102)'
                 gridSpots[i + 1].style.backgroundColor = 'rgb(102, 102, 102)'
                 gridSpots[i + 2].style.backgroundColor = 'rgb(102, 102, 102)'
@@ -32,6 +34,7 @@ const gameboard = () => {
         for (let i = 0; i < 3; i++) {
             if (gridSpotValues[i] != '' && gridSpotValues[i] == gridSpotValues[i + 3] && gridSpotValues[i] == gridSpotValues[i + 6]) {
                 console.log('pog');
+                heading.textContent = `${gridSpotValues[i]} won!`
                 gridSpots[i].style.backgroundColor = 'rgb(102, 102, 102)'
                 gridSpots[i + 3].style.backgroundColor = 'rgb(102, 102, 102)'
                 gridSpots[i + 6].style.backgroundColor = 'rgb(102, 102, 102)'
@@ -40,6 +43,7 @@ const gameboard = () => {
         }
         if ((gridSpotValues[0] != '' && gridSpotValues[0] == gridSpotValues[4] && gridSpotValues[0] == gridSpotValues[8])) {
             console.log('pog');
+            heading.textContent = `${gridSpotValues[4]} won!`
             gridSpots[0].style.backgroundColor = 'rgb(102, 102, 102)'
             gridSpots[4].style.backgroundColor = 'rgb(102, 102, 102)'
             gridSpots[8].style.backgroundColor = 'rgb(102, 102, 102)'
@@ -49,6 +53,10 @@ const gameboard = () => {
             gridSpots[2].style.backgroundColor = 'rgb(102, 102, 102)'
             gridSpots[4].style.backgroundColor = 'rgb(102, 102, 102)'
             gridSpots[6].style.backgroundColor = 'rgb(102, 102, 102)'
+            finished = true
+        }
+        if (playCounter == 9) {
+            heading.textContent = 'TIE'
             finished = true
         }
 
@@ -72,7 +80,6 @@ const gameboard = () => {
                     console.log(playCounter);
                     checkWin()
                 }
-                console.log(gridSpotValues);
 
             })
         })
